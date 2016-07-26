c54bdfbe7062eacf6a17c9cfb6c87ccd34516aa2
Константин Зыков <ricceron@yandex.ru>
2016-07-26 03:23:35 +0300

    Serializer implemented.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomSerializer;
+using System.IO;
+using System.Reflection;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        [Serializable]
+        class T
+        {
+            bool t = true;
+
+            public bool Get()
+            {
+                return t;
+            }
+        }
+
+        [Serializable]
+        struct s
+        {
+            public string Hello;
+        }
+
+        [Serializable]
+        class A
+        {
+            public int a;
+            public T t;
+        }
+
+        [Serializable]
+        class B : A
+        {
+            public decimal c;
+
+            public s e;
+
+            public string Get()
+            {
+                return "" + c + " " + a + " " + t.Get() + " " + e.Hello;
+            }
+                
+        }
+
+
+        static void Main(string[] args)
+        {
+            s s1 = new s();
+            s1.Hello = "OLOLO";
+            B b1 = new B();
+            b1.a = 42;
+            b1.c = 333;
+            b1.t = new T();
+            b1.e = s1;
+
+
+            Console.WriteLine(b1.GetType().FullName);
+            Console.WriteLine(b1.Get());
+            using (var ms = new MemoryStream())
+            {
+                Serializer.Serialize(b1, ms);
+                ms.Seek(0, SeekOrigin.Begin);
+                B b2 = Serializer.Deserialize(ms) as B;
+                Console.WriteLine(b2.Get());
+            }
+            Console.ReadKey();    
+
+
+        }
+    }
+}
---
+++ CustomSerializer/Serializer.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using static System.Reflection.BindingFlags;
+
+namespace CustomSerializer
+{
+    public static class Serializer
+    {
+        private static Dictionary<Type,byte> SimpleTypeCodes = new Dictionary<Type,byte>()
+        {
+            {typeof(byte),      0},
+            {typeof(sbyte),     1},         
+            {typeof(short),     2},
+            {typeof(ushort),    3},
+            {typeof(int),       4},
+            {typeof(uint),      5},
+            {typeof(long),      6},
+            {typeof(ulong),     7},
+            {typeof(float),     8},
+            {typeof(double),    9},
+            {typeof(decimal),   10},
+            {typeof(bool),      11},
+            {typeof(string),    12}
+        };
+
+        public static void Serialize(object graph, Stream stream)
+        {
+            if (graph == null)
+            {
+                throw new ArgumentNullException($"{nameof(graph)} is null");
+            }
+            if (stream == null)
+            {
+                throw new ArgumentNullException($"{nameof(stream)} is null");
+            }
+            if (!stream.CanWrite)
+            {
+                throw new ArgumentException($"{nameof(stream)} must be writeable.");
+            }
+
+            SerializeRecursive(graph, stream);
+        }
+
+        private static void SerializeRecursive(object obj, Stream stream)
+        {
+            var type = obj.GetType();
+            if (type.GetCustomAttributes(typeof(SerializableAttribute), true).Length > 0)
+            {
+                if (SimpleTypeCodes.Keys.Contains(type))
+                {
+                    SerializeSimple(obj, stream);
+                }
+                else
+                {
+                    string typeName = type.FullName;
+
+                    
+
+                    // Write type name
+                    var typeNameBytes = Encoding.Unicode.GetBytes(typeName);
+
+                    // Write type name length
+                    short typeLength = (short)typeNameBytes.Length;
+                    var typeLengthBytes = BitConverter.GetBytes(typeLength);
+                    stream.Write(typeLengthBytes, 0, typeLengthBytes.Length);
+
+                    stream.Write(typeNameBytes, 0, typeNameBytes.Length);
+
+                    var fields = type.GetAllFields();
+                   
+                    foreach (var field in fields)
+                    {
+                        // Write field
+                        var fieldValue = field.GetValue(obj);
+                        SerializeRecursive(fieldValue, stream);
+                    }
+                }
+                
+            }
+        }
+
+        private static void SerializeSimple(object obj, Stream stream)
+        {            
+            var type = obj.GetType();
+
+            // Two zero bytes means that type is simple.
+            stream.Write(BitConverter.GetBytes((short)0),0,2);
+
+            // Write simple type code
+            stream.WriteByte(SimpleTypeCodes[type]);
+
+            byte[] bytes = new byte[0];
+            switch(type.Name)
+            {
+                case "Sbyte":
+                    bytes = BitConverter.GetBytes((sbyte)obj);
+                    break;
+                case "Byte":
+                    bytes = BitConverter.GetBytes((byte)obj);
+                    break;
+                case "Int16":
+                    bytes = BitConverter.GetBytes((short)obj);
+                    break;
+                case "UInt16":
+                    bytes = BitConverter.GetBytes((ushort)obj);
+                    break;
+                case "Int32":
+                    bytes = BitConverter.GetBytes((int)obj);
+                    break;
+                case "UInt32":
+                    bytes = BitConverter.GetBytes((uint)obj);
+                    break;
+                case "Int64":
+                    bytes = BitConverter.GetBytes((long)obj);
+                    break;
+                case "UInt64":
+                    bytes = BitConverter.GetBytes((ulong)obj);
+                    break;
+                case "Single":
+                    bytes = BitConverter.GetBytes((float)obj);
+                    break;
+                case "Double":
+                    bytes = BitConverter.GetBytes((double)obj);
+                    break;
+                case "Decimal":
+                    int[] bits = decimal.GetBits((decimal)obj);
+                    List<byte> byteList = new List<byte>();
+                    foreach(var bitPart in bits)
+                    {
+                        byteList.AddRange(BitConverter.GetBytes(bitPart));
+                    }
+                    bytes = byteList.ToArray();
+                    break;
+                case "Boolean":
+                    bytes = BitConverter.GetBytes((bool)obj);
+                    break;
+                case "String":
+                    var stringBytes = new List<byte>();
+                    stringBytes.AddRange(BitConverter.GetBytes(((string)obj).Length));
+                    stringBytes.AddRange(Encoding.Unicode.GetBytes((string)obj));
+                    bytes = stringBytes.ToArray();
+                    break;                   
+            }
+            stream.Write(bytes, 0, bytes.Length);        
+        }
+
+        public  static object Deserialize(Stream stream)
+        {
+            if (stream == null)
+            {
+                throw new ArgumentNullException($"{nameof(stream)} is null");
+            }
+            if (!stream.CanRead)
+            {
+                throw new ArgumentException($"{nameof(stream)} must be readable.");
+            }
+
+            return DeserializeRecursive(stream);
+        }
+
+        private static object DeserializeRecursive(Stream stream)
+        {
+            var shortBuffer = new byte[2];
+            stream.Read(shortBuffer, 0, 2);
+            var length = BitConverter.ToInt16(shortBuffer,0);
+            if (length == 0)
+            {
+                return DeserializeSimple(stream);
+            }
+            var typeBuffer = new byte[length];
+            stream.Read(typeBuffer, 0, length);
+            string typeName = Encoding.Unicode.GetString(typeBuffer);
+            var asms = AppDomain.CurrentDomain.GetAssemblies();
+            IEnumerable<Type> types = null;
+            foreach(var assembly in asms)
+            {
+                types = assembly.GetTypes().Where(t => t.FullName == typeName);
+                if (types.Count() != 0)
+                {
+                    break;
+                }                   
+            }
+            if (types.Count() == 0)
+            {
+                throw new InvalidOperationException($"Can't find type {typeName} in current assembly.");
+            }
+            var type = types.First();
+            var result = Activator.CreateInstance(type);
+            var fields = type.GetAllFields();
+            foreach(var field in fields)
+            {
+                var value = DeserializeRecursive(stream);
+                field.SetValue(result, value);
+            }
+            return result;
+        }
+
+        private static object DeserializeSimple(Stream stream)
+        {
+            int intCode = stream.ReadByte();
+            byte code = (byte)intCode;
+            var type = SimpleTypeCodes.FirstOrDefault(x => x.Value == code).Key;
+            object result = null;
+            var buffer = new byte[32];
+            switch (type.Name)
+            {
+                case "Sbyte":
+                    result = (sbyte)stream.ReadByte();
+                    break;
+                case "Byte":
+                    result = stream.ReadByte();
+                    break;
+                case "Int16":
+                    stream.Read(buffer, 0, 2);
+                    result = BitConverter.ToInt16(buffer, 0);
+                    break;
+                case "UInt16":
+                    stream.Read(buffer, 0, 2);
+                    result = BitConverter.ToUInt16(buffer, 0);
+                    break;
+                case "Int32":
+                    stream.Read(buffer, 0, 4);
+                    result = BitConverter.ToInt32(buffer, 0);
+                    break;
+                case "UInt32":
+                    stream.Read(buffer, 0, 4);
+                    result = BitConverter.ToUInt32(buffer, 0);
+                    break;
+                case "Int64":
+                    stream.Read(buffer, 0, 8);
+                    result = BitConverter.ToInt32(buffer, 0);
+                    break;
+                case "UInt64":
+                    stream.Read(buffer, 0, 8);
+                    result = BitConverter.ToUInt32(buffer, 0);
+                    break;
+                case "Single":
+                    stream.Read(buffer, 0, 4);
+                    result = BitConverter.ToSingle(buffer, 0);
+                    break;
+                case "Double":
+                    stream.Read(buffer, 0, 8);
+                    result = BitConverter.ToDouble(buffer, 0);
+                    break;
+                case "Decimal":
+                    stream.Read(buffer, 0, 16);
+
+                    var i1 = BitConverter.ToInt32(buffer, 0);
+                    var i2 = BitConverter.ToInt32(buffer, 4);
+                    var i3 = BitConverter.ToInt32(buffer, 8);
+                    var i4 = BitConverter.ToInt32(buffer, 12);
+
+                    result =  new decimal(new int[] { i1, i2, i3, i4 });
+                    break;
+                case "Boolean":
+                    stream.Read(buffer, 0, 1);
+                    result = BitConverter.ToBoolean(buffer,0);
+                    break;
+                case "String":
+                    stream.Read(buffer, 0, 4);
+                    int length = BitConverter.ToInt32(buffer, 0);
+                    var stringBuffer = new byte[length * 2];
+                    stream.Read(stringBuffer, 0, length * 2);
+                    result = Encoding.Unicode.GetString(stringBuffer);
+                    break;
+            }
+            return result;
+        }
+
+        private static List<FieldInfo> GetAllFields(this Type type)
+        {
+            List<FieldInfo> fields = new List<FieldInfo>();
+            if (type.IsClass)
+            {
+                // Get all fields
+                List<Type> parentTypes = new List<Type>();
+                var parent = type;
+                while (!parent.Equals(typeof(object)))
+                {
+                    parentTypes.Add(parent);
+                    parent = parent.BaseType;
+                }
+                foreach (var pt in parentTypes)
+                {
+                    fields.AddRange(pt.GetFields(NonPublic));
+                    fields.AddRange(pt.GetFields());
+                }
+                fields = fields.Distinct(new TypeComparer()).ToList();
+            }
+            else
+            {
+                fields.AddRange(type.GetFields(NonPublic));
+                fields.AddRange(type.GetFields());
+            }
+            return fields;
+        }
+
+        class TypeComparer : IEqualityComparer<FieldInfo>
+        {
+            public bool Equals(FieldInfo x, FieldInfo y)
+            {
+                return (x.Name == y.Name);
+            }
+
+            public int GetHashCode(FieldInfo obj)
+            {
+                return 1;
+            }
+        }
+
+    }
+}
